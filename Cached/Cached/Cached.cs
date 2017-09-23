3cf7513b4a3be578656183bf257ade8308776ba1
David Garcinuño Enríquez <dagaren@gmail.com>
2017-09-24 00:55:44 +0200

    Added Cached. Base implementation of ICached

---
+++ Cached/Cached/Cached.cs
@@ -0,0 +1,45 @@
+﻿namespace Dagaren.Cached
+{
+    using System;
+
+    class Cached<T> : ICached<T>
+    {
+        private T value;
+
+        private bool isValid = false;
+
+        private Func<T> refreshFunction;
+
+        private readonly object lockObject = new object();
+
+        public Cached(Func<T> refreshFunction)
+        {
+            this.refreshFunction = refreshFunction ?? throw new ArgumentNullException("refreshFunction");
+        }
+
+        public T Value
+        {
+            get
+            {
+                if(this.isValid == false)
+                {
+                    lock(this.lockObject)
+                    {
+                        if(this.isValid == false)
+                        {
+                            this.value = this.refreshFunction.Invoke();
+                            this.isValid = true;
+                        }
+                    }
+                }
+
+                return this.value;
+            }
+        }
+
+        public void Invalidate()
+        {
+            this.isValid = false;
+        }
+    }
+}
